b60085535c7b43b33db669dd12826087b0156853
darshanrahate <rahate.darshan@gmail.com>
2025-08-26 08:27:08 +0530

    Initial Commit

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 12345
+let accountEmail = "abc@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 not allowed can not resign value
+
+console.log(accountId);
+
+accountEmail = "hc@hc.com"
+accountPassword = "1231231"
+accountCity = "Pune"
+
+
+
+console.table([accountId, accountEmail, accountPassword, accountState])
+
+
+
+
+
+
+
+
