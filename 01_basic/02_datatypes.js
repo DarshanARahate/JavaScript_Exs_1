1d1338905430deedde1f5bfa1c9326b04c3adbd5
darshanrahate <rahate.darshan@gmail.com>
2025-08-26 08:37:46 +0530

    Datatypes in Javascript

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -1,8 +1,13 @@
 
+// number => 2
+// bigint 
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined
+// symbol => unique
 
+// object
 
-
-
-
-
-
+console.log(typeof undefined)
+console.log(typeof null)
